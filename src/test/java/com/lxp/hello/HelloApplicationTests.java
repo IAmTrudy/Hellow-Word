30c221c02c21dc7e767161a9d37e449d21d944e6
Trudy <IAmTrudy@163.com>
2021-01-14 16:18:37 +0800

    项目结构初始化

--- src/test/java/com/lxp/hello/HelloApplicationTests.java
+++ src/test/java/com/lxp/hello/HelloApplicationTests.java
@@ -1,16 +1,10 @@
 package com.lxp.hello;
 
-import com.lxp.hello.bean.HelloBean;
-import org.apache.naming.factory.BeanFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
-
-import javax.annotation.Resource;
 
 @SpringBootTest
 class HelloApplicationTests {
